133578de3834d970a4325056aa1e522964148ada
bodyagun <bodyagun123@gmail.com>
2026-04-20 21:08:03 +0300

    1

---
+++ src/logic/todo.js
@@ -0,0 +1,46 @@
+function currentDate() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Add 1 because Jan is 0
+    const day = String(now.getDate()).padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+    return formattedDate
+}
+
+export default class Todo {
+    constructor(title, date, priority, status) {
+        
+        if (title === "") {
+            throw new Error("Title is required")
+        } else {
+            this.title = title.replace(/\s{2,}/g, ' ').trim();
+        }
+        
+        if (date === "") {
+            this.date = currentDate()
+        } else {
+            this.date = date
+        }
+
+        if (priority != "low" && priority != "high") {
+            this.priority = "low"
+        } else {
+            this.priority = priority
+        }
+
+        this.status = false
+        this.id = crypto.randomUUID()
+        }
+
+    changeValue(item, newValue) {
+        this[item] = newValue
+    }
+
+    toggleStatus() {
+        if (this.status === true) {
+            this.status = false
+        } else if (this.status === false) {
+            this.status = true
+        }
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,1 @@
+import Todo from "./logic/todo.js"
